be3d4786e11fb054cf514ddfe4667c59f88a8a27
lpsanger <lpssanger@gmail.com>
2024-03-14 20:02:47 -0300

    add authorization views nad refactor to controllers campground

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,39 +2,28 @@ const express = require('express');
 const router = express.Router({mergeParams: true});
 
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
 const Review = require('../models/review.js');
 const Campground = require('../models/campground.js');
+const {validateReview, isLoggedIn, isReviewAuthor} = require ('../middleware.js')
 
-const {reviewSchema} = require('../schemas.js');
 
 
-const validateReview = (req,res,next) =>{
-
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    }else{ 
-        next();
-    }
-}
-
-router.post('/', validateReview, catchAsync(async (req,res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req,res) => {
     const {id} = req.params;
     const campground = await Campground.findById(id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     
     await review.save();
     await campground.save();
 
-    req.flash('success', 'Created new campground!');
+    req.flash('success', 'New review added!');
 
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/:reviewId', catchAsync(async (req,res) =>{
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req,res) =>{
     const{id, reviewId} = req.params;
     await Campground.findByIdAndUpdate(id, {$pull :{reviews: reviewId}});
     await Review.findByIdAndDelete(reviewId);
--- seeds/index.js
+++ seeds/index.js
@@ -17,6 +17,7 @@ const seedDB = async () =>{
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random()* 20) + 10;
         const camp  = new Campground({
+            author: '65ee02c547057cd879c3db64',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
