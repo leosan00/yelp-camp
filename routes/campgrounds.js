7b884c0f2bcd5bc534994505cc263bd08496cd72
lpsanger <lpssanger@gmail.com>
2024-03-18 16:54:16 -0300

    refactor to controllers files, edited routes and new star form for reviews

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -9,19 +9,19 @@ const campgrounds = require ('../controllers/campgrounds');
 
 const {isLoggedIn, validateCampground, isAuthor} = require ('../middleware.js')
 
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-router.get('/', catchAsync(campgrounds.index))
+router.get('/new',isLoggedIn, campgrounds.renderNewForm);
 
-router.get('/new',isLoggedIn, campgrounds.renderNewForm)
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-router.post('/',isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-router.get('/:id', catchAsync(campgrounds.showCampground));
 
 router.get('/:id/edit',isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router.put('/:id',isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
