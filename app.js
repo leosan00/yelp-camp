75ad52eca9630eb4ddbd958fbdd731184adad45c
lpsanger <lpssanger@gmail.com>
2024-03-19 19:12:33 -0300

    add env file, cloudinary upload, delete iamges, input file, multer npm

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_EV !== "production"){
+    require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
