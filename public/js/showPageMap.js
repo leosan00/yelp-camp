637ef9fa9bc1f4d6cfcf3e72cd14ee86b786d347
lpsanger <lpssanger@gmail.com>
2024-03-20 12:57:03 -0300

    add mapBox sdk, location map for all campgrounds

---
+++ public/js/showPageMap.js
@@ -0,0 +1,15 @@
+  // TO MAKE THE MAP APPEAR YOU MUST
+  // ADD YOUR ACCESS TOKEN FROM
+
+  // https://account.mapbox.com
+  mapboxgl.accessToken = mapToken;
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/light-v10', // style URL
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 9, // starting zoom
+  });
+
+  new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(new mapboxgl.Popup().setHTML(`<h3>${campground.title}</h3><p> ${campground.location}</p>`)).addTo(map);
